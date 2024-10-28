a4008df549330fead0a71f8fd49f258bc7619668
Narcis Matas <146827232+narcismatas@users.noreply.github.com>
2024-10-28 15:52:52 +0100

    Melodia i Text

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
 basic.forever(function () {
-	
+    if (input.temperature() > 22) {
+        basic.showString("Calor")
+    } else {
+        basic.showString("Fred")
+    }
+    if (input.lightLevel() > 130) {
+        music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.UntilDone)
+    } else {
+        music.play(music.stringPlayable("C5 A B G A F G E ", 120), music.PlaybackMode.UntilDone)
+    }
 })
